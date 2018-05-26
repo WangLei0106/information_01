f82b448fd201ff908fc5e6a94309df831e2cc09a
manage <manag@163.com>
2018-05-26 09:36:46 +0800

    修改完成

--- login.py
+++ login.py
@@ -1,22 +1,5 @@
-<<<<<<< HEAD
-from flask import Flask
-
-# 创建app
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-
-    return 'index'
-
-
-if __name__ == '__main__':
-
-    app.run()
-=======
 num = 10
 num = 20
 num = 30
 num = 40
 
->>>>>>> d370626f385e0bf3e4e97d78bfd1d0534b1fde67
